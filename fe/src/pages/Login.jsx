9f0bad2bee8e419b9acf5e957611aa56e85f61bf
sungjoon92 <wsjtjdwns92@naver.com>
2025-03-19 11:03:06 +0900

    [FR-11] feat: 로그인페이지구현
    - 로그인 페이지 레이아웃 구현
    - 배너 이미지 홈에서만 노출
    - 배너, 로고 이미지 /public 경로 제거 (public 경로는 이미 이 위치에
      있기떄문에 필요가없음)

--- fe/src/pages/Login.jsx
+++ fe/src/pages/Login.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 function Login() {
   return (
-    <div>
-      로그인 페이지야~~~~~~~~~
-      <Link to="/signup" >회원가입 페이지 이동</Link>
-    </div>
+    <>
+      <form className="flex flex-col text-center">
+        <input className="text-center" type="text" placeholder="아이디" />
+        <input className="text-center" type="password" placeholder="비밀번호" />
+        <button type="submit">로그인</button>
+        <button>카카오 로그인</button>
+        <Link to="/signup">회원가입</Link>
+      </form>
+    </>
   );
 }
 
