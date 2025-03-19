67d142caeece6ca09065ef5a0f9d2425fec66874
wag192625 <wag192625@gmail.com>
2025-03-19 15:40:48 +0900

    [FR-19] feat: 이메일/닉네임 검증 api 응답 데이터 수정

--- be/src/main/java/com/forpets/be/domain/user/controller/UserController.java
+++ be/src/main/java/com/forpets/be/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.forpets.be.domain.user.controller;
 
 import com.forpets.be.domain.user.dto.response.UserCheckResponseDto;
 import com.forpets.be.domain.user.service.UserService;
+import com.forpets.be.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +18,17 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/auth/email-verifications")
-    public ResponseEntity<UserCheckResponseDto> checkUsername(@RequestParam String username) {
-        return ResponseEntity.ok(userService.checkUsername(username));
+    public ResponseEntity<ApiResponse<UserCheckResponseDto>> checkUsername(
+        @RequestParam String username) {
+        return ResponseEntity.ok(
+            ApiResponse.ok("이메일 중복 검증 성공", "OK", userService.checkUsername(username)));
     }
 
     @GetMapping("/auth/nickname-verifications")
-    public ResponseEntity<UserCheckResponseDto> checkNickname(@RequestParam String nickname) {
-        return ResponseEntity.ok(userService.checkNickname(nickname));
+    public ResponseEntity<ApiResponse<UserCheckResponseDto>> checkNickname(
+        @RequestParam String nickname) {
+        return ResponseEntity.ok(
+            ApiResponse.ok("닉네임 중복 검증 성공", "OK", userService.checkNickname(nickname)));
     }
 
 }
