67e1ce88b1e156352eb8b50209e9a266cd683ea6
Choi Wonnho <wonnho@nate.com>
2025-03-21 19:19:27 +0900

    [FR-22] fix: 봉사자 등록글 응답 데이터로 게시글 id 추가

--- be/src/main/java/com/forpets/be/domain/servicevolunteer/dto/response/ServiceVolunteerResponseDto.java
+++ be/src/main/java/com/forpets/be/domain/servicevolunteer/dto/response/ServiceVolunteerResponseDto.java
@@ -7,28 +7,28 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class ServiceVolunteerResponseDto {
 
-    private String title;
-    private AnimalType animalType;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private String departureArea;
-    private String arrivalArea;
-    private String notice;
-    private LocalDateTime createdAt;// from BaseTimeEntity
-    private LocalDateTime updatedAt;
+    private final Long id;
+    private final String title;
+    private final AnimalType animalType;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final String departureArea;
+    private final String arrivalArea;
+    private final String notice;
+    private final LocalDateTime createdAt;// from BaseTimeEntity
+    private final LocalDateTime updatedAt;
 
     // Static factory method to map from entity to DTO
     public static ServiceVolunteerResponseDto from(ServiceVolunteer entity) {
         return
             ServiceVolunteerResponseDto.builder()
+                .id(entity.getId())
                 .title(entity.getTitle())
                 .animalType(entity.getAnimalType())
                 .departureArea(entity.getDepartureArea())
