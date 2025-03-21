dd7a31d3343422023f5be30334188231418e9be2
Choi Wonnho <wonnho@nate.com>
2025-03-21 19:07:10 +0900

    [FR-22] fix: JPARepository에서 String을 Long오로 변경

--- be/src/main/java/com/forpets/be/domain/servicevolunteer/repository/VolunteerRepository.java
+++ be/src/main/java/com/forpets/be/domain/servicevolunteer/repository/VolunteerRepository.java
@@ -3,7 +3,7 @@ package com.forpets.be.domain.servicevolunteer.repository;
 import com.forpets.be.domain.servicevolunteer.entity.ServiceVolunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VolunteerRepository extends JpaRepository<ServiceVolunteer, String> {
+public interface VolunteerRepository extends JpaRepository<ServiceVolunteer, Long> {
 
 
 }
