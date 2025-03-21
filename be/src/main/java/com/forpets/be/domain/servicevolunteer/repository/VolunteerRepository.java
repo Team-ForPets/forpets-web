9082f43c3b6a5b0246077556ed26f5e8ad88b5f4
Choi Wonnho <wonnho@nate.com>
2025-03-21 18:22:08 +0900

    [FR-22] fix: frontend에서 불필요한 a.html을 삭제함

---
+++ be/src/main/java/com/forpets/be/domain/servicevolunteer/repository/VolunteerRepository.java
@@ -0,0 +1,9 @@
+package com.forpets.be.domain.servicevolunteer.repository;
+
+import com.forpets.be.domain.servicevolunteer.entity.ServiceVolunteer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VolunteerRepository extends JpaRepository<ServiceVolunteer, String> {
+
+
+}
