348e65fdf6e97eef1e358b5249ad795868a40ee8
kska01 <insideka01@gmail.com>
2025-03-24 18:59:24 +0900

    FR-48 [BE] 로그아웃 예외처리 (#38)

--- be/src/main/java/com/forpets/be/global/config/SecurityConfig.java
+++ be/src/main/java/com/forpets/be/global/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/logout").authenticated()
                 .requestMatchers("/api/auth/**", "/error").permitAll()
                 .anyRequest().authenticated()
             )
