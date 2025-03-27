dabd489996cad472937f646688fb8d3d943b93c7
Choi Wonnho <wonnho@nate.com>
2025-03-27 16:03:13 +0900

    [FR-61] feat: 봉사자 글 등록 재등록

--- be/src/main/java/com/forpets/be/domain/servicevolunteer/service/VolunteerService.java
+++ be/src/main/java/com/forpets/be/domain/servicevolunteer/service/VolunteerService.java
@@ -1,10 +1,13 @@
 package com.forpets.be.domain.servicevolunteer.service;
 
 import com.forpets.be.domain.servicevolunteer.dto.request.ServiceVolunteerRequestDto;
+import com.forpets.be.domain.servicevolunteer.dto.response.ServiceVolunteerListResponseDto;
 import com.forpets.be.domain.servicevolunteer.dto.response.ServiceVolunteerResponseDto;
 import com.forpets.be.domain.servicevolunteer.entity.ServiceVolunteer;
 import com.forpets.be.domain.servicevolunteer.repository.VolunteerRepository;
 import com.forpets.be.domain.user.entity.User;
+import com.forpets.be.domain.user.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class VolunteerService {
 
     private final VolunteerRepository volunteerRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public ServiceVolunteerResponseDto createVolunteer(ServiceVolunteerRequestDto requestDto,
@@ -25,5 +29,23 @@ public class VolunteerService {
         volunteer.addUser(user);
 
         return ServiceVolunteerResponseDto.from(volunteerRepository.save(volunteer));
+    }
+
+    public List<ServiceVolunteerListResponseDto> getAllVolunteers() {
+
+        return volunteerRepository.findAll().stream()
+
+            .map(volunteer -> {
+
+// Fetch associated User
+
+                User user = volunteer.getUser();
+
+                return ServiceVolunteerListResponseDto.from(volunteer, user);
+
+            })
+
+            .toList();
+
     }
 }
