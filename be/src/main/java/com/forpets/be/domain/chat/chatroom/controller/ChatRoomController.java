9d3e424a1ca58dacf0d4a0d0df44f16953243cac
SangHo Lee <steve0312@naver.com>
2025-03-28 13:59:45 +0900

    [FR-83] feat: 채팅방 상세 조회 API 구현
    - 봉사자 또는 요청자 닉네임, 출발 가능 지역 또는 출발 요청 지역, 도착 가능 지역 또는 도착 요청 지역, 나의 아이 정보, 방 상태(시작 전/진행 중/완료), 채팅 메시지 내역 조회
      - 내가 요청자로 참여하는지 봉사자로 참여하는지에 따라 닉네임, 출발지, 도착지 내용이 달라지게 설정
    - 필드로 데이터를 응답하지 않는 데이터에 대해서는 DTO 형태로 생성해서 응답 (ChatMessageResponseDto)

--- be/src/main/java/com/forpets/be/domain/chat/chatroom/controller/ChatRoomController.java
+++ be/src/main/java/com/forpets/be/domain/chat/chatroom/controller/ChatRoomController.java
@@ -1,6 +1,7 @@
 package com.forpets.be.domain.chat.chatroom.controller;
 
 import com.forpets.be.domain.chat.chatroom.dto.request.ChatRoomRequestDto;
+import com.forpets.be.domain.chat.chatroom.dto.response.ChatRoomDetailResponseDto;
 import com.forpets.be.domain.chat.chatroom.dto.response.ChatRoomResponseDto;
 import com.forpets.be.domain.chat.chatroom.dto.response.RequestorChatRoomsListResponseDto;
 import com.forpets.be.domain.chat.chatroom.dto.response.VolunteerChatRoomsListResponseDto;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +50,13 @@ public class ChatRoomController {
         @RequestParam Long volunteerId) {
         return ResponseEntity.ok(ApiResponse.ok("봉사자로 속한 채팅방 리스트가 조회되었습니다.", "OK",
             chatRoomService.getVolunteerChatRooms(volunteerId)));
+    }
+
+    // 채팅방 개별 조회
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<ApiResponse<ChatRoomDetailResponseDto>> getChatRoom(
+        @PathVariable Long chatRoomId) {
+        return ResponseEntity.ok(ApiResponse.ok(chatRoomId + "번 채팅방이 조회되었습니다.", "OK",
+            chatRoomService.getChatRoomById(chatRoomId)));
     }
 }
