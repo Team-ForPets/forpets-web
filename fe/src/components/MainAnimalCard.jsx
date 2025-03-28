a89166c90de32814153ede8ca459a6473b949867
성준 <130916219+sungjoon92@users.noreply.github.com>
2025-03-28 11:52:42 +0900

    FR-79 [FE] 메인페이지 봉사자 마커 구현 (#68)
    - 메인페이지 요청자, 봉사자 텝 클릭시 그에맞는 출발지 주소 마커구현 완료
    - 텝 클릭시 주황색으로 활성

--- fe/src/components/MainAnimalCard.jsx
+++ fe/src/components/MainAnimalCard.jsx
@@ -4,15 +4,15 @@ function MainAnimalCard({ animal }) {
   const { departureArea, arrivalArea, animalType, weight, imageUrl } = animal;
 
   return (
-    <li className="flex items-center gap-5 p-3 border rounded-md box-border bg-[#ffffff]">
+    <li className="w-full h-[150px]  flex items-center gap-5 p-3 border rounded-md box-border bg-[#ffffff]">
       <div className="w-[100px] ">
-        <img className="w-[100px]" src={`assets/logo.png`} alt="나의아이 이미지" />
+        <img className="w-[100px] object-cover" src={`assets/logo.png`} alt="나의아이 이미지" />
         {/* 이미지 값 들어오면 변경예정 */}
         {/* <img src={`${imageUrl}`} alt="나의아이 이미지" /> */}
       </div>
       <ul className="text-[12px]">
-        <li>출발: {departureArea}</li>
-        <li>도착: {arrivalArea}</li>
+        <li>출발지: {departureArea}</li>
+        <li>도착지: {arrivalArea}</li>
         <li>품종: {animalType === 'DOG' ? '개' : animalType === 'CAT' ? '고양이' : '기타'}</li>
         <li>체중: {weight}kg</li>
       </ul>
