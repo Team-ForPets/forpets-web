6e3a676258408b3684cf2735d3d81d81131b009d
wag192625 <106229215+wag192625@users.noreply.github.com>
2025-03-31 12:32:19 +0900

    [FR-107] chore: 배포 테스트 (#88)
    - 강사님과 함께

--- be/src/main/java/com/forpets/be/global/security/handler/JwtAuthenticationEntryPoint.java
+++ be/src/main/java/com/forpets/be/global/security/handler/JwtAuthenticationEntryPoint.java
@@ -5,11 +5,14 @@ import com.forpets.be.global.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -21,7 +24,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
+        log.error("401");
+        log.trace(Arrays.toString(authException.getStackTrace()));
         ApiResponse<Void> errorResponse = ApiResponse.error("인증이 필요합니다.", "UNAUTHORIZED");
         response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
