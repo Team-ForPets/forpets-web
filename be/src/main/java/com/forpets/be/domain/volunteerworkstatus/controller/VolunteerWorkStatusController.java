47b3779a9785c3c3660986d78051e757c4f3bf66
SangHo Lee <steve0312@naver.com>
2025-03-31 15:59:30 +0900

    [FR-111] feat: 나의 이동봉사 현황 조회 API 구현
    - 로그인한 사용자가 요청자 또는 봉사자로 참여했던 이동봉사 현황 조회하는 로직 구현
    - 로그인한 사용자가 요청자 또는 봉사자로 참여했던 이동봉사 현황이 없는 경우를 검증하는 로직 구현

--- be/src/main/java/com/forpets/be/domain/volunteerworkstatus/controller/VolunteerWorkStatusController.java
+++ be/src/main/java/com/forpets/be/domain/volunteerworkstatus/controller/VolunteerWorkStatusController.java
@@ -1,5 +1,6 @@
 package com.forpets.be.domain.volunteerworkstatus.controller;
 
+import com.forpets.be.domain.user.entity.User;
 import com.forpets.be.domain.volunteerworkstatus.dto.request.VolunteerWorkStatusRequestDto;
 import com.forpets.be.domain.volunteerworkstatus.dto.response.VolunteerWorkStatusListResponseDto;
 import com.forpets.be.domain.volunteerworkstatus.dto.response.VolunteerWorkStatusResponseDto;
@@ -8,6 +9,7 @@ import com.forpets.be.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,5 +39,13 @@ public class VolunteerWorkStatusController {
         return ResponseEntity.ok(
             ApiResponse.ok("이동봉사 현황 리스트가 조회되었습니다.", "OK",
                 volunteerWorkStatusService.getVolunteerWorkStatuses(status)));
+    }
+
+    // 나의 이동봉사 현황 조회
+    @GetMapping("/my/volunteer-work-status")
+    public ResponseEntity<ApiResponse<VolunteerWorkStatusListResponseDto>> getMyVolunteerWorkStatus(
+        @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(ApiResponse.ok(user.getNickname() + "님의 이동봉사 현황이 조회되었습니다.", "OK",
+            volunteerWorkStatusService.getMyVolunteerWorkStatus(user)));
     }
 }
