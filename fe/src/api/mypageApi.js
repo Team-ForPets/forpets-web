3be95730ee77c95fb2c0e8469549ca86bb145044
SangHo Lee <steve0312@naver.com>
2025-04-03 03:23:32 +0900

    [FR-145] feat: 마이페이지 프로필 수정 버튼 클릭 시 비밀번호 검증 기능 구현
    - 수정 버튼 클릭 후 모달창에서 사용자가 입력한 비밀번호 데이터를 백엔드로 보내서 일치 여부 검증

--- fe/src/api/mypageApi.js
+++ fe/src/api/mypageApi.js
@@ -7,8 +7,14 @@ const mypageApi = {
     const response = await api.get(`${ENDPOINT}/profile`);
     return response.data;
   },
+  // 사용자별 나의 아이 정보 조회
   getMyAnimals: async () => {
     const response = await api.get(`${ENDPOINT}/animals`);
+    return response.data;
+  },
+  // 프로필 수정 시 비밀번호 검증
+  getCheckPassword: async (data) => {
+    const response = await api.post(`${ENDPOINT}/profile/password-verifications`, data);
     return response.data;
   },
 };
