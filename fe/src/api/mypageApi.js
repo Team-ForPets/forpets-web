9d56079cfa3ca8d3be1ee5d53e42a6672b14849d
wag192625 <wag192625@gmail.com>
2025-04-03 21:01:54 +0900

    [FR-150] feat: api 일단 불러오기까진 성공

--- fe/src/api/mypageApi.js
+++ fe/src/api/mypageApi.js
@@ -11,6 +11,10 @@ const mypageApi = {
     const response = await api.get(`${ENDPOINT}/animals`);
     return response.data;
   },
+  getMyVolunteerPost: async () => {
+    const response = await api.get(`${ENDPOINT}/volunteer-posts`, { withCredentials: true });
+    return response;
+  },
 };
 
 export default mypageApi;
